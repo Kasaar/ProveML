133f2bce99f56e26e26ded5df5ea581f7e906da1
zelle105 <zelle105@umn.edu>
2023-11-18 19:18:24 -0600

    Add equation and declaration to ast.
    Initial addition of equation and declaration datatypes to ast with the
    expectation that they will need to be modified.

--- ProveML/lib/ast.ml
+++ ProveML/lib/ast.ml
@@ -1,3 +1,11 @@
 type expression =
         | Identifier of string
         | Application of expression * expression
+
+type equation =
+        | Operator of string
+        | Type of expression * expression
+        | Equation of expression * equation * expression
+
+type declaration =
+        | Let of expression * equation
